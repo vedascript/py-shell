10c243796272827a0754c4b573f9266c5e2a50b3
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-01 21:05:59 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    # REPL is achieved using input
-    command = input()
-    print(f"{command}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        # REPL is achieved using input
+        command = input()
+        print(f"{command}: command not found")
     
 if __name__ == "__main__":
     main()
