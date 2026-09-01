33c2e2d6c59d88ac9f13752be1cca607bac4416a
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-01 20:36:11 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,10 +2,7 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    # sys.stdout.write("$ ")
-    pass
-
-
+    sys.stdout.write("$ ")
+    
 if __name__ == "__main__":
     main()
