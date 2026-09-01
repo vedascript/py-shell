756488ffda9d05af75c1054683a6ba509055217c
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-01 20:48:41 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    command = sys.argv[1]
+    # REPL is achieved using input
+    command = input()
     print(f"{command}: command not found")
     
 if __name__ == "__main__":
