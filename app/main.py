8ed9ee4306a42599e45e2317f7abd036df6d53a5
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-01 20:44:57 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    command = sys.argv[0]
+    command = sys.argv[1]
     print(f"{command}: command not found")
     
 if __name__ == "__main__":
