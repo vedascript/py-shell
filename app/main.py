681548bc726c6a6c803155f60f231046f2f12dc8
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-05 19:35:43 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -131,10 +131,8 @@ def main():
                 continue;
 
             parsed_file_path_input = handle_input_string(f"'{file_path_input}'");
-            print(f"parsed path: {parsed_file_path_input}")
             final_path = os.getcwd() + parsed_file_path_input;
             if(os.path.exists(final_path) and os.path.isfile(final_path)):
-                print("reading file")
                 with open(final_path) as file:
                     content = file.read();
                     file_contents += content;    
