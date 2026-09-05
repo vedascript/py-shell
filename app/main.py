5983f9a343faee3f034b2e02bfde7dffc668d344
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-05 19:45:48 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -134,9 +134,9 @@ def main():
             print(f"parsed path: {parsed_file_path_input}")
             final_path = os.getcwd() + parsed_file_path_input;
             print(f"final+path: {final_path}")
-            print(f"does path exists: {os.path.exists(final_path)}")
-            print(f"isFile: {os.path.isfile(final_path)}")
-            if(os.path.exists(final_path) and os.path.isfile(final_path)):
+            print(f"does path exists: {os.path.exists(parsed_file_path_input)}")
+            print(f"isFile: {os.path.isfile(parsed_file_path_input)}")
+            if(os.path.exists(parsed_file_path_input) and os.path.isfile(parsed_file_path_input)):
                 print("reading file")
                 with open(final_path) as file:
                     content = file.read();
