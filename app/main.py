cbe5b33d596f60a7e52d900a9630199fca3b58f2
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-06 13:31:11 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ def handle_input_string(input_str):
     for str in str_arr:
         if(not str.strip() and len(str)):
             parsed_input_str += " ";
-        elif(str[0] == " "):
+        elif(len(str) and str[0] == " "):
             striped_str = str.lstrip();
             parsed_input_str+= f'{" " + striped_str}'    
         else:    
