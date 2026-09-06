5de14963b7c3b9d48c1ff28f4c6f08382ab6919b
Vedansh Wani <me.vedanshwani@gmail.com>
2026-09-06 13:24:20 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -52,7 +52,7 @@ def get_path_type(path):
 def handle_input_string(input_str):
     parsed_input_str = "";
     str_arr = input_str.split('"');
-    print(f'str_arr {str_arr}')
+
     for str in str_arr:
         if(not str.strip() and len(str)):
             parsed_input_str += " ";
